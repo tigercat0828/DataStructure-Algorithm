fbc4650a3293271807887fb5bee2eff28681ede5
Tigercat <tigercat0828@gmail.com>
2022-07-18 09:02:21 +0800

    new AvlTree

---
+++ DSALGO/DataStructures/BinarySearchTree/AvlNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeco.DataStructures.BinarySearchTree {
+    public class AvlNode<T> {
+        public AvlNode(T key, AvlNode<T> left = null, AvlNode<T> right = null) {
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            factor = 0;
+            height = 0;
+        }
+        public AvlNode<T> left;
+        public AvlNode<T> right;
+        public T key;
+        public int height;
+        public int factor;
+    }
+}
