4e0e969069d07a036478eda5f62fbcce7848393c
Tigercat <tigercat0828@gmail.com>
2022-07-31 18:38:21 +0800

    test markdown image

--- DSALGO/DataStructure/Graph/GraphMatrix.cs
+++ DSALGO/DataStructure/Graph/GraphMatrix.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DSALGO.DataStructure.Graph {
     public class GraphMatrix : IGraph {
         const double X = 10000000;
-        public bool isUndirected => throw new NotImplementedException();
+        public bool isUndirected { get; }
         public int NodeCount => Graph[0].Count;
         public List<List<double>> Graph;
-        public GraphMatrix(int nodeCount) {
+        public GraphMatrix(int nodeCount, bool isUndirected) {
+            this.isUndirected = isUndirected;
             Graph = new();
             for (int i = 0; i < nodeCount; i++) {
                 Graph[i] = new List<double> {};
             }
         }
-        public GraphMatrix(int nodeCount, List<Edge> edges) : this(nodeCount){
+        public GraphMatrix(int nodeCount, bool isUndirected ,List<Edge> edges) : this(nodeCount, isUndirected) {
             foreach (var edge in edges) {
                 AddEdge(edge);
             }
@@ -53,38 +56,97 @@ namespace DSALGO.DataStructure.Graph {
             return Graph[from][to] == X;
         }
 
-        public bool ContainsNode(int node) => node >= NodeCount;
+        public bool ContainsNode(int node) => node < NodeCount;
 
         public void DeleteEdge(int from, int to) {
-            throw new NotImplementedException();
+            if (!ContainsEdge(from, to))
+                throw new Exception($"Edge ({from}, {to}) is not in the graph");
+            Graph[from][to] = X;
         }
 
         public void DeleteNode(int node) {
-            throw new NotImplementedException();
+            for (int i = 0; i < NodeCount; i++) {
+                Graph[node][i] = X;
+                Graph[i][node] = X;
+            }
         }
 
         public void EditEdge(int from, int to, double newWeight) {
-            throw new NotImplementedException();
+            if (!ContainsEdge(from, to)) {
+                throw new NotImplementedException();
+            }
+            Graph[from][to] = newWeight;
         }
 
         public List<Edge> GetAdjEdges(int node) {
-            throw new NotImplementedException();
+            List<Edge> edges = new();
+            for (int i = 0; i < NodeCount; i++) {
+                double wei = Graph[node][i];
+                if (wei!= X) {
+                    edges.Add(new Edge(node, i, wei));
+                }
+            }
+            return edges;
         }
 
         public List<int> GetAdjNodes(int node) {
-            throw new NotImplementedException();
+            List<int> nodes= new();
+            for (int i = 0; i < NodeCount; i++) {
+                double wei = Graph[node][i];
+                if (wei != X) {
+                    nodes.Add(i);
+                }
+            }
+            return nodes;
         }
 
         public List<int> GetAllNodes() {
-            throw new NotImplementedException();
+            List<int> nodes = new();
+            for (int i = 0; i < NodeCount; i++) {
+                if (Graph[i][i] != X) {
+                    nodes.Add(i);
+                }
+            }
+            return nodes;
         }
 
         public void Print() {
-            throw new NotImplementedException();
+            foreach (var row in Graph) {
+                row.Print();
+            }
         }
 
         public List<Edge> ToEdgeList() {
-            throw new NotImplementedException();
+            if (isUndirected) return GetUndirectedEdges();
+            return GetDirectedEdges();
+        }
+
+        private List<Edge> GetDirectedEdges() {
+            List<Edge> edges = new();
+            for (int i = 0; i < Graph.Count; i++) {
+                for (int j = 0; j < Graph[i].Count; j++) {
+                    double wei = Graph[i][j];
+                    if(wei != X) {
+                        edges.Add(new Edge(i, j, wei));
+                    }
+                }
+            }
+            return edges;
+        }
+
+        private List<Edge> GetUndirectedEdges() {
+            List<Edge> edges = new();
+            for (int i = 0; i < Graph.Count; i++) {
+                for (int j = 0; j < Graph[i].Count; j++) {
+                    if (j > i) {
+                        double wei = Graph[i][j];
+                        if (wei != X) {
+                            edges.Add(new Edge(i, j, wei));
+                        }
+                    }
+                }
+            }
+            return edges;
         }
     }
 }
