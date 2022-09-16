572cfa1d7112ef72633d6af21542b6218617dc1b
Tigercat <tigercat0828@gmail.com>
2022-09-16 17:20:23 +0800

    Matrix Chain

---
+++ DSALGO/Algorithm/DynamicProgramming/MatrixChain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSALGO.Algorithm.DynamicProgramming {
+    public class MatrixChain {
+
+        // M[i,j] = min i<=k<j{ M[i,k] + M[k+1,j] + e[i-1] * e[k] * e[j] };
+        int[,] M;
+        int[,] S;
+        int count = 0;
+        public (int, int) MinOperateCount(int[] P) {
+            int n = P.Length;
+
+            AllocateSpace(n);
+
+            for (int i = 1; i <= n - 1; i++) {
+                for (int j = 1; j <= n - 1 - i; j++) {
+                    int I = j;
+                    int J = j + i;
+                    //Console.Write($"M[{R}, {C}]   ");
+                    for (int k = I; k < J; k++) {
+                        
+                        int next = M[I, k] + M[k + 1, J] + P[I - 1] * P[k] * P[J];
+                        if (next < M[I, J]) {
+                            M[I, J] = next;
+                            S[I, J] = k;
+                        }
+                        count++;
+                    }
+                }
+                //Console.WriteLine();
+            }
+            return (M[1, n - 1], 1);
+        }
+        public void PrintTable() {
+            Console.WriteLine("DP table");
+            for (int i = 1; i < M.GetLength(0); i++) {
+                Console.Write("|");
+                for (int j = 1; j < M.GetLength(1); j++) {
+                    if (i <= j) {
+                        Console.Write($"{M[i, j],6} |");
+                    }
+                    else {
+                        Console.Write("       |");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Optimal table");
+            for (int i = 1; i < M.GetLength(0); i++) {
+                Console.Write("|");
+                for (int j = 1; j < M.GetLength(1); j++) {
+                    if (i <= j) {
+                        Console.Write($"{S[i, j],2} |");
+                    }
+                    else {
+                        Console.Write("   |");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public void AllocateSpace(int n) {
+            M = new int[n, n];
+            S= new int[n, n];
+            for (int i = 0; i < M.GetLength(0); i++) {
+                for (int j = 0; j < M.GetLength(1); j++) {
+                    M[i, j] = int.MaxValue;
+                }
+            }
+            for (int i = 0; i < M.GetLength(0); i++) {
+                M[i, i] = 0;
+            }
+            
+        }
+    }
+}
--- DSALGO/Program.cs
+++ DSALGO/Program.cs
@@ -1,32 +1,10 @@
+using DSALGO.Algorithm.DynamicProgramming;
 using DSALGO.Algorithm.GraphTheory.CycleAndSCC;
 using DSALGO.DataStructure.GraphStructure;
 using System.Reflection.Metadata.Ecma335;
 using static DSALGO.Utility;
-
-Graph graph = new Graph(8);
-
-graph.AddEdge(0, 1);
-graph.AddEdge(0, 2);
-graph.AddEdge(1, 0);
-graph.AddEdge(1, 3);
-graph.AddEdge(2, 3);
-graph.AddEdge(3, 4);
-graph.AddEdge(3, 5);
-graph.AddEdge(4, 2);
-graph.AddEdge(4, 5);
-graph.AddEdge(4, 6);
-graph.AddEdge(5, 7);
-graph.AddEdge(6, 5);
-graph.AddEdge(7, 6);
-
-
-graph.Print();
-DrawBoldLine();
-KosarajuSCC kosa = new();
-TarjanSCC tar = new();
-
-kosa.FindSCCs(graph).Print();
-DrawBoldLine();
-tar.FindSCCs(graph).Print();
-
-
+//int[] P = new int[] { 4, 10, 3, 12, 20, 7 };
+int[] P = new int[] { 30,35,15,5,10,20,25 };
+MatrixChain chain = new();
+Console.WriteLine(chain.MinOperateCount(P));
+chain.PrintTable();
